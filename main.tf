16e8d30adc9a930838b29d664e91552cff65ab3e
Jeff Destine <jdestine@ocrolus.com>
2021-06-10 11:26:41 -0400

    all

---
+++ main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+provider "aws" {
+   region = var.region
+}
+
+resource "aws_kms_key" "key" {
+    description = "key to encrypt bucket"
+    policy      = jsonencode(
+      {
+        Id      = "key-default-1"
+        Statement = [
+          {
+            Action: [
+             "kms:Encrypt", 
+             "kms:Decrypt",
+             "kms:ReEncrypt*",
+             "kms:GenerateDataKey",
+             "kms:GenerateDataKey*",
+             "kms:DescribeKey"
+            ],
+          }
+        ]
+      }
+    )
+}
+
+resource "aws_s3_bucket" "bucket" {
+    bucket      = var.name
+    acl         = var.acl
+
+    versioning {
+    enabled = false
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+       kms_master_key_id = aws_kms_key.key.arn
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
+}
