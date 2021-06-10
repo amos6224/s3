93bf6a59be74dd8e7c717965f0fa7c7b65cdecc7
Jeff Destine <Jeff.destine@axonius.com>
2021-06-10 14:59:19 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_version = ">= 0.12"
-}
-
-provider "aws" {
-   region = var.region
-}
-
 resource "aws_kms_key" "key" {
     description = "key to encrypt bucket"
     policy      = jsonencode(
